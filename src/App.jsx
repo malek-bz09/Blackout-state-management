7930f1e2a0b34924e443fe2de1303a7928908704
malek-bz09 <abdelmalek.aidoussi@etu.usthb.dz>
2026-08-11 02:44:06 +0100

    refactor: update profile form to manage state and handle submissions

--- src/App.jsx
+++ src/App.jsx
@@ -4,45 +4,25 @@ import Addcard from './components/profileform.jsx'
 import './App.css'
 
 function App() {
-const profiles = [
-  {
-    id: 1,
-    name: "Abdelmalek",
-    role: "Frontend Developer",
-    skills: ["JavaScript", "React", "Git"]
-  },
-  {
-    id: 2,
-    name: "Sarah",
-    role: "UI/UX Designer",
-    skills: ["Figma", "UI Design", "Prototyping"]
-  },
-  {
-    id: 3,
-    name: "Yacine",
-    role: "Backend Developer",
-    skills: ["Python", "FastAPI", "MySQL"]
-  },
-  {
-    id: 4,
-    name: "Lina",
-    role: "Full Stack Developer",
-    skills: ["React", "Node.js", "MongoDB"]
-  },
-  {
-    id: 5,
-    name: "Amine",
-    role: "Cybersecurity Student",
-    skills: ["Linux", "Networking", "Python"]
-  }
-];
+
+
+const [profiles, setProfiles] = useState([]);
 
 
   return(
-  <>
-   <Addcard />
+  <section className="app">
+
+   <div className="navbar">
+     <h1> TEAM MANAGER </h1>
+   </div>
+
+   <Addcard   
+   setProfiles={setProfiles} 
+   />
+
    <ProfileList profiles= {profiles} />
-  </>
+
+  </section>
   );
 }
 
--- src/components/profileform.jsx
+++ src/components/profileform.jsx
@@ -1,19 +1,60 @@
 import "./profileform.css";
+import { useState } from "react";
 
-function Addcard() {
+function Addcard({ setProfiles }) {
+
+  const [profile, setProfile] = useState({
+    name: "",
+    role: "",
+    skills: ""
+  });
+
+  function addProfile() {
+    setProfiles(prev => [
+      ...prev,
+      {
+        ...profile,
+        id: Date.now(),
+        skills: profile.skills.split(",")
+      }
+    ]);
+  }
+
   return (
 <div className="form-card">
   <h2>Add New Profile</h2>
 
   <div className="form-row">
     <div className="field">
+
       <label>Name</label>
-      <input type="text" placeholder="Enter full name" />
+      <input 
+      type="text"   
+      value={profile.name}
+      onChange={(e) =>
+    setProfile({
+      ...profile,
+      name: e.target.value
+    })}  placeholder="Enter full name" />
+
     </div>
 
     <div className="field">
+
+
+
       <label>Role</label>
-      <input type="text" placeholder="Enter role / position" />
+      <input 
+       type="text"  
+       value={profile.role}
+       onChange={(e) =>
+       setProfile({
+      ...profile,
+      role: e.target.value
+      })} 
+      placeholder="Enter role / position" />
+
+
     </div>
 
     <div className="field">
@@ -21,10 +62,16 @@ function Addcard() {
       <input
         type="text"
         placeholder="React, JavaScript, Git"
+        value={profile.skills}
+        onChange={(e) =>
+        setProfile({
+      ...profile,
+        skills: e.target.value
+    })}
       />
     </div>
 
-    <button>Add Profile</button>
+    <button onClick={addProfile}>Add Profile</button>
   </div>
 </div>
   );
